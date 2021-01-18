137d19d4af8c16496e0413e3c01e1fb2fe4dbb22
geovbox <46532304+geovbox@users.noreply.github.com>
2021-01-18 14:04:58 +0800

    Update install-hugo.sh
    修改为0.68.3版本

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NAME=hugo
 REPO=https://github.com/gohugoio/hugo
-VERSION=0.68
+VERSION=0.68.3
 
 curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
