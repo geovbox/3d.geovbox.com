7a25f253c39f58876c5ec4ad39236e47446a4010
geovbox <46532304+geovbox@users.noreply.github.com>
2021-01-18 14:21:57 +0800

    Update install-hugo.sh
    改为hugo_extended_0.68.3_Linux-64bit.tar.gz

--- install-hugo.sh
+++ install-hugo.sh
@@ -3,4 +3,4 @@ NAME=hugo
 REPO=https://github.com/gohugoio/hugo
 VERSION=0.68.3
 
-curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
+curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_extended_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
